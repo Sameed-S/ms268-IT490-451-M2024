0f58de17aecc4523cf2590297a5282297c2162c8
Ubuntu <ubuntu@ip-172-31-20-21.ec2.internal>
2024-05-29 23:41:35 +0000

    Commting the changes i made for the solution for module 2 hw

--- RabbitMQServerSample.php
+++ RabbitMQServerSample.php
@@ -4,29 +4,45 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-function login($user,$pass){
-	//TODO validate user credentials
-	return true;
+function login($user, $pass) {
+    // TODO: validate user credentials
+    return true;
 }
 
-function request_processor($req){
-	echo "Received Request".PHP_EOL;
-	echo "<pre>" . var_dump($req) . "</pre>";
-	if(!isset($req['type'])){
-		return "Error: unsupported message type";
-	}
-	//Handle message type
-	$type = $req['type'];
-	switch($type){
-		case "login":
-			return login($req['username'], $req['password']);
-		case "validate_session":
-			return validate($req['session_id']);
-		case "echo":
-			return array("return_code"=>'0', "message"=>"Echo: " .$req["message"]);
-	}
-	return array("return_code" => '0',
-		"message" => "Server received request and processed it");
+function request_processor($req) {
+    echo "Received Request" . PHP_EOL;
+    echo "<pre>" . var_dump($req) . "</pre>";
+
+    if (!isset($req['type'])) {
+        echo "Error: unsupported message type" . PHP_EOL;
+        return "Error: unsupported message type";
+    }
+
+    // Handle message type
+    $type = $req['type'];
+    $response = array("return_code" => '0', "message" => "Server received request and processed it");
+
+    switch ($type) {
+        case "login":
+            $response = login($req['username'], $req['password']);
+            break;
+        case "validate_session":
+            $response = validate($req['session_id']);
+            break;
+        case "echo":
+            $response = array("return_code" => '0', "message" => "Echo: " . $req["message"]);
+            break;
+        default:
+            echo "Error: unsupported message type" . PHP_EOL;
+            return "Error: unsupported message type";
+    }
+
+    
+    echo "Valid Response sent: " . PHP_EOL;
+    print_r($response);
+    echo PHP_EOL;
+
+    return $response;
 }
 
 $server = new rabbitMQServer("testRabbitMQ.ini", "sampleServer");
@@ -36,3 +52,4 @@ $server->process_requests('request_processor');
 echo "Rabbit MQ Server Stop" . PHP_EOL;
 exit();
 ?>
+
